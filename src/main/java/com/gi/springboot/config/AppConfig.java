a394aa024110f6cd7705dc31a225542ed6ed012d
gopikrishhnachokkalamani <gopikrishhnachokkalamani@gopikrihhnasmbp>
2018-03-08 15:59:04 -0500

    updated, few more ways to load multiple yaml files in springboot

--- src/main/java/com/gi/springboot/config/AppConfig.java
+++ src/main/java/com/gi/springboot/config/AppConfig.java
@@ -10,11 +10,30 @@ import org.springframework.core.io.ClassPathResource;
 public class AppConfig {
 
 	@Bean
-	public static PropertySourcesPlaceholderConfigurer properties() {
+	public PropertySourcesPlaceholderConfigurer properties() {
 		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
 		YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+		
+		//OPTION 1 - Load Individual Yaml Files
 		yaml.setResources(new ClassPathResource("student.yml"), new ClassPathResource("address.yml"));
 		// yaml.setResources(new ClassPathResource("address.yml"));
+		
+		//OPTION 2 - Load All Yaml Files
+		//ClassLoader cl = this.getClass().getClassLoader();
+		//ResourcePatternResolver rpResolver = new PathMatchingResourcePatternResolver(cl);
+		//Resource configFiles = rpResolver.getResource("classpath*:/*.yml");
+		//yaml.setResources(configFiles);
+		
+		//OPTION 3 - Using Spring Profiles
+		//rename your yaml files to application-student.yml, application-address.yml
+		//Comment this config file completely
+		//Add        spring:
+		//		        profiles:
+		//		        	    include: 
+		//		        	    	   - student
+		//		        	    	   - address 
+		//		  in application.yml
+		
 		propertySourcesPlaceholderConfigurer.setProperties(yaml.getObject());
 		return propertySourcesPlaceholderConfigurer;
 	}
